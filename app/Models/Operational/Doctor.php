7259fc1f6519560900642d77e168eda773f61bb0
maulanairfanf <maulanairfanf@gmail.com>
2024-03-19 13:06:17 +0700

    create model

---
+++ app/Models/Operational/Doctor.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models\Operational;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Doctor extends Model
+{
+    // use HasFactory;
+    use SoftDeletes;
+
+    public $table = 'doctor';
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'specialist_id',
+        'name',
+        'fee',
+        'photo',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+}
